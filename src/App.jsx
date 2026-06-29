4a6a327d4ee1c2ca73e058ecb24938b1b311074b
Brooke F <brooke.floyd10@gmail.com>
2026-06-29 17:05:48 -0500

    added components to app.jsx, set current genre and useState in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+
 import './App.css';
+import GenreList from './components/GenreList';
+import MovieList from './components/MovieList';
 
 function App() {
-	return <></>;
+	const [currentGenre, setCurrentGenre] = useState('');
+	const []
+	return 
+	<>
+	<GenreList />
+
+	<MovieList />
+
+	</>;
 }
 
 export default App;
